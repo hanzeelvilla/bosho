f3953510c1bc9e40098431544f4772867a67ca2b
Hanzeel <hanzeelvilla0@gmail.com>
2024-06-01 17:13:18 -0600

    push grande

---
+++ BOSHO/BOSHO.ino
@@ -0,0 +1,367 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+#include "D:\Hanzeel\FaCUMtad\Semestre 4\proyecto_esli\MQTT\MQTT.ino" //RUTA DEL ARCHIVO MQTT
+
+//MOTORES
+#define AIN1 15
+#define AIN2 2
+#define BIN1 4
+#define BIN2 5
+
+bool izquierdaActivo = false;
+bool derechaActivo = false;
+bool adelanteActivo = false;
+bool atrasActivo = false;
+
+//Sensor seguidor de línea
+#define sensD 34
+#define sensI 35
+
+//ULTRASONICO
+#define TRIGGER 33
+#define ECHO 32
+#define SOUND_SPEED 0.034
+long duration;
+bool ultraSonicoActivo = false; // Variable para el estado del sensor
+
+//BOCHOMOVIL
+#define buzzer 23
+#define ledD 19
+#define ledI 21
+
+//SERVIDOR
+
+const char* ssid = "INFINITUM02A0";
+const char* password = "RACbe21akl";
+WebServer server(80);  
+
+//MQTT
+ MQTT mqtt;
+ long lastMsg = 0;
+
+void evitar_obstaculos() {
+  digitalWrite(TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER, LOW);
+
+  duration = pulseIn(ECHO, HIGH);
+  float distancia = duration * SOUND_SPEED/2;
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+  
+  if(distancia < 10) {
+    atras(255);
+    delay(1000);
+    derecha(255);
+    delay(1000);
+  } else {
+    adelante(255);
+  }
+}
+
+void seguidor_linea(){
+    int valorD = analogRead(sensD);
+    int valorI = analogRead(sensI);
+
+    //INTENTO CON VELOCIDAD A 240 
+    if(valorD < 4000 && valorI < 4000){
+      adelante(220);
+      Serial.println("Adelante");
+    }
+    else if(valorD > 4000 && valorI < 4000){
+      izquierda(210);
+      itermitenteI();
+      Serial.println("Izquierda");
+    }
+    else if(valorI > 4000 && valorD < 4000){
+      derecha(210);
+      itermitenteD();
+      Serial.println("Derecha");
+    }
+  }
+
+void tusa(){
+  tone(buzzer, 880);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, LOW);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 740);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, HIGH);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 659.25);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, LOW);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 587.33);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, HIGH);
+  delay(700);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, LOW);
+  noTone(1000);
+  delay(500);
+
+  tone(buzzer, 587.33);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, HIGH);
+  delay(300);
+  noTone(500);
+  delay(250);
+
+  tone(buzzer, 587.33);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, HIGH);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 659.25);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, LOW);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 740);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, HIGH);
+  delay(300);
+  noTone(500);
+  tone(buzzer, 880);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, HIGH);
+  delay(700);
+  noTone(500);
+  
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, LOW);
+  noTone(1000);
+  delay(500);
+  tone(buzzer, 880);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, HIGH);
+  delay(700);
+  noTone(500);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, LOW);
+}
+
+void autodestruccion(){
+  for(int i=0; i<3; i++){
+    tone(buzzer, 1000);
+    digitalWrite(ledI, HIGH);
+    digitalWrite(ledD, HIGH);
+    delay(500);
+    noTone(buzzer);
+    digitalWrite(ledI, LOW);
+    digitalWrite(ledD, LOW);
+    delay(500);
+  }
+  
+  for(int i=0; i<5; i++){
+    tone(buzzer, 1000);
+    digitalWrite(ledI, HIGH);
+    digitalWrite(ledD, HIGH);
+    delay(300);
+    noTone(buzzer);
+    digitalWrite(ledI, LOW);
+    digitalWrite(ledD, LOW);
+    delay(300);
+  }
+
+  for(int i=0; i<10; i++){
+    tone(buzzer, 1000);
+    digitalWrite(ledI, HIGH);
+    digitalWrite(ledD, HIGH);
+    delay(90);
+    noTone(buzzer);
+    digitalWrite(ledI, LOW);
+    digitalWrite(ledD, LOW);
+    delay(90);
+  }
+
+  tone(buzzer, 1000);
+  digitalWrite(ledI, HIGH);
+  digitalWrite(ledD, HIGH);
+  delay(3000);
+  noTone(buzzer);
+  digitalWrite(ledI, LOW);
+  digitalWrite(ledD, LOW);
+  delay(2000);
+}
+
+ void parar(){
+    analogWrite(AIN1, 0); 
+    analogWrite(AIN2, 0);
+    analogWrite(BIN1, 0); 
+    analogWrite(BIN2, 0);
+  } 
+
+  void adelante(int velocidad){
+    analogWrite(AIN1, velocidad);  //-10
+    analogWrite(AIN2, LOW);
+    analogWrite(BIN1, velocidad); 
+    analogWrite(BIN2, LOW);
+  }
+
+  void atras(int velocidad){
+    analogWrite(AIN1, LOW); 
+    analogWrite(AIN2, velocidad);
+    analogWrite(BIN1, LOW); 
+    analogWrite(BIN2, velocidad);
+  }
+
+  void derecha(int velocidad){
+    analogWrite(AIN1, velocidad); 
+    analogWrite(AIN2, LOW);
+    analogWrite(BIN1, LOW); 
+    analogWrite(BIN2, LOW);
+  }
+
+  void izquierda(int velocidad){
+    analogWrite(AIN1, LOW); 
+    analogWrite(AIN2, LOW);
+    analogWrite(BIN1, velocidad); 
+    analogWrite(BIN2, LOW);
+  }
+
+  void diagonalIzquierda(int velocidad) {
+  analogWrite(AIN1, velocidad); //-20
+  analogWrite(AIN2, LOW);
+  analogWrite(BIN1, velocidad); 
+  analogWrite(BIN2, LOW);
+}
+
+void diagonalDerecha(int velocidad) {
+  analogWrite(AIN1, velocidad); 
+  analogWrite(AIN2, LOW);
+  analogWrite(BIN1, velocidad); //-40
+  analogWrite(BIN2, LOW);
+}
+
+void diagonalIzquierdaA(int velocidad) {
+  analogWrite(AIN1, LOW); 
+  analogWrite(AIN2, velocidad);
+  analogWrite(BIN1, LOW); 
+  analogWrite(BIN2, velocidad); //-20
+}
+
+void diagonalDerechaA(int velocidad) {
+  analogWrite(AIN1, LOW); 
+  analogWrite(AIN2, velocidad); //-60
+  analogWrite(BIN1, LOW); 
+  analogWrite(BIN2, velocidad);
+}
+
+  void probar_motores(int velocidad){
+    adelante(velocidad);
+    delay(1000);
+    parar();
+    delay(1000);
+    atras(velocidad);
+    delay(1000);
+    parar();
+    delay(1000);
+    derecha(velocidad);
+    delay(1000);
+    parar();
+    delay(1000);
+    izquierda(velocidad);
+    delay(1000);
+    parar();
+    delay(1000);
+    parar();
+    delay(1000);
+  }
+
+  void print_valores_seguidor(int valD, int valI){
+    Serial.print("Derecha: ");
+    Serial.println(valD);
+    Serial.print("Izquierda: ");
+    Serial.println(valI);
+    delay(1000);
+  }
+
+  void itermitenteD() {
+    digitalWrite(ledD, HIGH);
+    delay(200);
+    digitalWrite(ledD, LOW);
+  }
+
+  void itermitenteI() {
+    digitalWrite(ledI, HIGH);
+    delay(200);
+    digitalWrite(ledI, LOW);
+  }
+
+void setup() {
+  Serial.begin(115200);
+  
+  mqtt.setup_WiFi ( );
+  mqtt.set_MQTT_server ( );
+  mqtt.set_MQTT_callback (  );\
+
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  
+  pinMode(sensD, INPUT);
+  pinMode(sensI, INPUT);
+
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT); 
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(ledI, OUTPUT);
+  pinMode(ledD, OUTPUT);
+
+  //tusa();
+}
+ 
+void loop() {
+  delay ( 10 );
+  mqtt.reconnect_MQTT ( );
+  long now = millis();
+    if (now - lastMsg > 5000) {
+      lastMsg = now;
+      
+      mqtt.publish_MQTT ( );
+    }
+
+  if (adelanteActivo && izquierdaActivo) {
+    diagonalDerecha(240);
+  } else if (adelanteActivo && derechaActivo) {
+    diagonalIzquierda(240);
+  } else if (atrasActivo && izquierdaActivo) {
+    diagonalIzquierdaA(240);
+  } else if (atrasActivo && derechaActivo) {
+    diagonalDerechaA(240);
+  } else if (adelanteActivo) {
+    adelante(240);
+  } else if (atrasActivo) {
+    atras(220);
+  } else if (izquierdaActivo) {
+    izquierda(240);
+    itermitenteI();
+  } else if (derechaActivo) {
+    derecha(220);
+    itermitenteD();
+  } else {
+    parar();
+  }
+
+  //delay(200);
+  /*
+  iniciarTrigger();
+  distanceCm = calcularDistancia();
+  Serial.println(distanceCm);
+  */
+
+  //print_valores_seguidor(valorD, valorI); 
+  
+  //seguidor_linea();
+}
---
+++ MQTT/MQTT.ino
@@ -0,0 +1,221 @@
+//Librería para convertir a JSON el mensaje recibido
+#include <ArduinoJson.h>
+
+/* Se incluye librería y se crea objeto para la configuración del cliente WiFi */
+#include <WiFi.h>
+  WiFiClient ESP32_WIFI;
+
+/* Se incluye librería paa utilizar el protocolo de telemetría MQTT, con el objeto correspondiente */
+#include "PubSubClient.h"
+  PubSubClient client ( ESP32_WIFI );
+
+/* Directivas de configuración de red y parámetros MQTT */
+#define HOTSPOT_WIFI  "Wokwi-GUEST"            /* Nombre del punto de acceso a conectarse */
+#define HOTSPOT_PWD   ""                       /* Contraseña de la red */
+#define MQTT_SERVER   "test.mosquitto.org"     /* Dirección del Broker MQTT */
+#define MQTT_PORT     1883                     /* Puerto del Broker MQTT */
+#define TXTOPIC       "/TX_ELBOSHO"         /* Nombre del topic de publicación */
+#define RXTOPIC       "/RX_ELBOSHO"         /* Nombre del topic de suscripción */
+
+float latitud = 19.256878;
+float longitud = -103.752708;
+
+//VARIABLES EXTERNAS
+extern bool ultraSonicoActivo; 
+extern bool izquierdaActivo;
+extern bool derechaActivo;
+extern bool adelanteActivo;
+extern bool atrasActivo;
+
+/* Clase para la gestión de la red WiFi y el protocolo MQTT */
+class MQTT {
+  
+  public:
+  /* MÉTODOS */
+  public:
+    void subscribe_MQTT ( void );            /* Conecta al usuario como un suscriptor MQTT */
+    void publish_MQTT ( void );              /* Conecta al usuario como un pulicador MQTT */
+    void reconnect_MQTT ( void );            /* Realiza la reconexión al servidor MQTT */
+    void setup_WiFi ( void );                /* Configura lo necesario para conectarse a una red WiFi */ 
+    void set_MQTT_server ( void );           /* Establece la dirección del Broker MQTT a utilizar */
+    void set_MQTT_callback ( void );         /* Se configura la obtención de la información como suscriptor */
+    static void callback ( char* , byte* , unsigned int ); /* Método que obtiene la trama realizada en las suscripciones */
+
+  };
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ set_MQTT_server ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que configura el puerto y la dirección del Broker MQTT a utilizar.                     *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: set_MQTT_server ( void ) {
+
+  client.setServer ( MQTT_SERVER, MQTT_PORT );            /* Función librería PubsubClient */
+  
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ set_MQTT_callback ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que configura la función que recibirá la trama proveniente de los datos              *
+*                      mostrados en la suscripción .                                                                *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: set_MQTT_callback ( void ) {
+
+  client.setCallback (this -> callback);                 /* Función librería PubsubClient */
+
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ subscribe_MQTT ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que se suscribe a un topic MQTT, puede tener múltiples suscripciones.                *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: subscribe_MQTT ( void ) {
+
+  client.subscribe( RXTOPIC );                           /* Función librería PubsubClient */
+  //client.subscribe( RXTOPIC1 );                          /* Función librería PubsubClient */
+  
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ publish_MQTT ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que publica información hacia a un topic MQTT.                                       *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: publish_MQTT ( void ) {
+
+ Serial.println(F("Publicando información"));
+
+  // Crear un documento JSON
+  DynamicJsonDocument doc(1024);
+  doc["lat"] = latitud;
+  doc["lgt"] = longitud;
+
+  // Convertir el documento JSON a una cadena
+  char buffer[512];
+  size_t n = serializeJson(doc, buffer);
+
+  // Publicar la cadena JSON en el topic
+  client.publish(TXTOPIC, buffer, n);
+   
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ reconnect_MQTT ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que se reeconecta al servidor MQTT como cliente, tanto para publicar como suscribirse*
+*                      a un topic.                                                                                  *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: reconnect_MQTT ( void ) {
+
+  if ( ! client.connected ( ) ) {                         /* ¿Está el cliente desconectado? */
+
+    while ( ! client.connected ( ) ) {                    /* Mientras que está desconectado */
+      
+      Serial.print ( F ( "Conectando nuevamente al servidor" ) );
+
+      if ( client.connect ( "Client_test" ) ) {             /* Si el cliente se conecta, el cliente puede tener autentificación */
+        
+        Serial.println ( F ( "Cliente conectado" ) );
+        
+        subscribe_MQTT ( );                                 /* Suscribirse al topic */
+        
+      } else {    
+                                                  /* Si el cliente no se conecta */
+        Serial.print ( F ( "Falló al conectarse, tipo de error: " ) );
+        Serial.print ( client.state ( ) );                  /* Mostrar el estado o error, e intentarlo nuevamente */
+        Serial.println ( F ( "Intentando nuevamente en 5 segundos" ) );
+          delay ( 5000 );
+
+        }
+    }
+  }
+  client.loop ( );
+  
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ setup_WiFi ( void ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que se configura y se conecta a una red WiFi                                         *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: setup_WiFi ( void ) {
+
+  delay ( 10 );
+  Serial.println ( F ( "Configurando WiFi: " ) );
+  WiFi.begin ( HOTSPOT_WIFI, HOTSPOT_PWD );               /* Iniciar intentos de conexión hacia la red WiFi */
+  
+  
+  while ( WiFi.status ( ) != WL_CONNECTED ) {            /* Mientras que no se haya conectado a la red especificada */
+    
+    delay ( 500 );
+    Serial.print ( F ( "." ) );                                /* Mostrar un mensaje de espera */ 
+    
+  }
+  //Serial.println ( "WiFi configurado con la IP: " );
+  
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ callback ( char* topic, byte* message, unsigned int length ) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*                      Función que obtiene los mensajes mandados hacia el topic al cual se está suscrito                                                      *
+*                      NOTA: Los paráetros de está función están de acuerdo con los que vienen en la librería Pubsubclient                                    *
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+void MQTT :: callback(char* topic, byte* message, unsigned int length) {
+
+  Serial.print ( F ( "Ha llegado un mensaje de: " ) );
+  Serial.println ( topic );                             /* Topic al cual se está suscrito */
+  
+  String messageTemp;
+  
+  for ( int i = 0; i < length; i++ ) {
+    //Serial.print((char)message[i]);
+    messageTemp += (char)message[i];                    /* El mensaje proviene en bytes por lo que es necesario concatenarlo */
+  }
+
+  Serial.println( messageTemp );
+
+
+  //TRANSFORMAR EL MENSAJE
+  DynamicJsonDocument doc(1024);
+
+  DeserializationError error = deserializeJson(doc, messageTemp);
+
+  if (error) {
+    Serial.print(F("Error parseando JSON: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  //GUARDAR LA INFO DEL JSON
+  bool ultraSonico = doc["ultraSonico"];
+  bool izquierda = doc["izquierda"];
+  bool derecha = doc["derecha"];
+  bool adelante = doc["adelante"];
+  bool atras = doc["atras"];
+  
+  //CONDICIONES
+  if (izquierda) {
+    izquierdaActivo = true;
+    Serial.println("Izquierda");
+  } else {
+    izquierdaActivo = false;
+  }
+
+  if (derecha) {
+    derechaActivo = true;
+    Serial.println("Derecha");
+  } else {
+    derechaActivo = false;
+  }
+
+  if (adelante) {
+    adelanteActivo = true;
+    Serial.println("Adelante");
+  } else {
+    adelanteActivo = false;
+  }
+
+  if (atras) {
+    atrasActivo = true;
+    Serial.println("Atras");
+  } else {
+    atrasActivo = false;
+  }
+
+  }
